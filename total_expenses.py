4d133880ec330994924c596bbf6eb2492254fe6e
Jalilov <javohirjalilov777@gmail.com>
2022-09-27 13:37:02 +0500

    added tasks

---
+++ total_expenses.py
@@ -0,0 +1,10 @@
+# Calculate the total expenses
+def total_expenses(monthly_expenses: dict) -> int:
+    """
+    Calculate the total expenses
+    Args:
+        monthly_expenses: dictionary of monthly expenses
+    Returns:
+        total_expenses: total expenses
+    """
+    return 
